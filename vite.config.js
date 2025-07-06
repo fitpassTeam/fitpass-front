ed2bd68f59744937b58ba65d457fef570b208204
hosung77 <hosung77>
2025-07-06 05:11:13 +0000

    fix:에러수정"

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
