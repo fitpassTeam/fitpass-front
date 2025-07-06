bc05c421b18c3b1c469995f425667c6a0c112b7e
hosung77 <hosung77>
2025-07-06 05:08:33 +0000

    fix:에러 수정"

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_BASE_URL,
+        // eslint-disable-next-line no-undef
+        target: process.env.VITE_API_BASE_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/ws': {
-        target: import.meta.env.VITE_API_BASE_URL,
+        // eslint-disable-next-line no-undef
+        target: process.env.VITE_API_BASE_URL,
         changeOrigin: true,
         ws: true,
       },
